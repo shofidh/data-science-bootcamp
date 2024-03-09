2711756cdf88fbe7cf55f16373ca51207bb84420
Shofi Dh <138829982+shofidh@users.noreply.github.com>
2024-03-09 21:02:10 +0700

    Update primes.py
    menambah coment syarat bilangan prima dan penjelasan bagian input

--- primes.py
+++ primes.py
@@ -3,6 +3,7 @@ def check_prime(n):
       print(f"check_prime({n}) = Please input positive integer and more than 1 !")
     else :
       prime = True
+        #syarat bilangan prima
       for i in range (2,int(n**0.5)+1):
         if n % i == 0 :
           prime = False
@@ -12,6 +13,7 @@ def check_prime(n):
       else:
         print(f"check_prime({n}) =  {n} is not prime")
 
+#input nilai, bisa lebih dari satu
 arr_value = [0,1,2,3,4,5,6,9,10,6]
 
 for n in arr_value:
